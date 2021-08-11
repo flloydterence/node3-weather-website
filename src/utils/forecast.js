6548aa1dd69fab3f6c8777d7cb890427ef0f80aa
Flloyd De Leon <flloydterence@yahoo.com.ph>
2021-08-11 11:24:52 +0800

    Changed unit to celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `http://api.weatherstack.com/current?access_key=8db5d1f475e206806dcf687a3b153520&query=${latitude},${longitude}&units=f`;
+  const url = `http://api.weatherstack.com/current?access_key=8db5d1f475e206806dcf687a3b153520&query=${latitude},${longitude}`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
